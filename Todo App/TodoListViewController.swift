68dfcc7b8640db7e168c02840654a8faa325058f
Song-Yun Wang <songyunwus@gmail.com>
2024-09-05 18:52:50 -0700

    Add checkmark function

--- Todo App/TodoListViewController.swift
+++ Todo App/TodoListViewController.swift
@@ -24,6 +24,22 @@ class TodoListViewController: UITableViewController {
         cell.textLabel?.text = itemArray[indexPath.row]
         return cell
     }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        print(itemArray[indexPath.row])
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+    }
+    
+//    override func tableView(_ tableView: UITableView, willDeselectRowAt indexPath: IndexPath) -> IndexPath? {
+//        <#code#>
+//    }
 
 }
 
